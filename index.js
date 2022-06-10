b4b9fda159316341466bfff575508fa8032218c2
Aurélien <au2001sfg@gmail.com>
2022-06-10 19:07:13 +0200

    Create /api route

--- index.js
+++ index.js
@@ -11,5 +11,12 @@ app.get("/", (req, res) => {
   res.send("Hello, World!\n")
 })
 
+app.get("/api", (req, res) => {
+  res.type("json")
+  res.send(JSON.stringify({
+    hello: "world"
+  }))
+})
+
 app.listen(PORT, HOST)
 console.log(`Running on http://${HOST}:${PORT}`)
